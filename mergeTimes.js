fb6da3e3b6a5cf3a3de4bbf6178c966dbf20239c
Jeremiah <JeremiahBerndt@gmail.com>
2022-01-31 11:09:48 -0700

    initialized

---
+++ mergeTimes.js
@@ -0,0 +1,13 @@
+// Write a function merge_ranges() that takes a list of multiple meeting time ranges and returns a list of condensed ranges.
+
+// For example, given:
+
+//   [(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)]
+
+// your function would return:
+
+//   [(0, 1), (3, 8), (9, 12)]
+
+const mergeRanges = () => {
+
+}
