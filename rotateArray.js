3ea0a1ca65c3055ad23f432ed59540d81d867da5
Jeremiah <JeremiahBerndt@gmail.com>
2022-02-06 14:57:44 -0700

    rotate array left and right

--- rotateArray.js
+++ rotateArray.js
@@ -1,7 +1,17 @@
-const rotate = (arr, count = 1) => {
+// rotateLeft
+const rotateLeft = (arr, count = 1) => {
   return arr.slice(count, arr.length).concat(arr.slice(0, count));
 };
 
+// rotateRight
+function rotateRight(arr, count) {
+  count -= arr.length * Math.floor(count / arr.length);
+  let leftSide = arr.slice(0, arr.length - count)
+  let rightSide = arr.slice(arr.length - count)
+  return rightSide.concat(leftSide);
+  // return arr;
+}
+
 const arr = [1, 2, 3, 4, 5];
 
-console.log(rotate(arr, 1));  // [2, 3, 4, 5, 1]
+console.log(rotateRight(arr, 11));  // [2, 3, 4, 5, 1]
