5f4d06ea7d1518117488d10a25697c0a219afdb3
Jeremiah <JeremiahBerndt@gmail.com>
2022-02-19 17:15:13 -0700

    letterCombinations complete

--- letterCombinations.js
+++ letterCombinations.js
@@ -1,66 +1,48 @@
+// const letterCombinations = (digits) => {
+//   //edge case
+//   if (digits == null || digits.length === 0) return [];
+//   // define a map to use for each digit
 const map = {
-  '2': ['a', 'b', 'c'],
-  '3': ['d', 'e', 'f'],
-  '4': ['g', 'h', 'i'],
-  '5': ['j', 'k', 'l'],
-  '6': ['m', 'n', 'o'],
-  '7': ['p', 'q', 'r', 's'],
-  '8': ['t', 'u', 'v'],
-  '9': ['w', 'x', 'y', 'z']
-}
-
-const letterCombinations = (digits) => {
-  //edge case
-  if (digits == null || digits.length === 0) return [];
-  // define a map to use for each digit
-  const map = {
-    2: 'abc',
-    3: 'def',
-    4: 'ghi',
-    5: 'jkl',
-    6: 'mno',
-    7: 'pqrs',
-    8: 'tuv',
-    9: 'wxyz',
-  };
-  // this is a result array and a function for iterating
-  const res = [];
-  const go = (i, s) => {
-    // if we have just added the last digit, we know one combination is complete
-    // so we push it to result
-    if (i === digits.length) {
-      res.push(s);
-      return;
-    }
-    // we begin a recursive call with i = 0
-    for (const c of map[digits[i]]) {
-      go(i + 1, s + c);
-    }
-  };
-
-  go(0, '');
-  return res;
+  2: 'abc',
+  3: 'def',
+  4: 'ghi',
+  5: 'jkl',
+  6: 'mno',
+  7: 'pqrs',
+  8: 'tuv',
+  9: 'wxyz',
 };
+//   // this is a result array and a function for iterating
+//   const res = [];
+//   const go = (i, s) => {
+//     // if we have just added the last digit, we know one combination is complete
+//     // so we push it to result
+//     if (i === digits.length) {
+//       res.push(s);
+//       return;
+//     }
+//     // we begin a recursive call with i = 0
+//     for (const c of map[digits[i]]) {
+//       go(i + 1, s + c);
+//     }
+//   };
 
-// const combinations = [];
-
-// var letterCombinations = function (digits, combo = '') {
-//   // [a, b, c] => [d, e, f]
-//   if (digits.length === 0) {
-//     combinations.push(combo);
-//     return;
-//   }
-//   for (let i = 0; i < digits.length; i++) {
-//     // each number from the digits
-//     let letters = map[digits[i]];
-//     letters.forEach(letter => {
-//       //each letter associated with 1 digit
-//       combo += letter;
-//       letterCombinations(digits.substring(1), combo)
-//       combo = combo.substring(0, combo.length - 1)
-//     })
-//   }
-//   return combinations;
+//   go(0, '');
+//   return res;
 // };
 
-console.log(letterCombinations('257'))
+const combinations = [];
+// i refers to the current index of digits
+
+var letterCombinations = (digits, combo = '', i = 0) => {
+  if (digits === null || !digits.length) { return [] }
+  if (digits.length === i) {
+    return combinations.push(combo);
+  }
+  for (let char of map[digits[i]]) {
+    letterCombinations(digits, combo + char, i + 1);
+  };
+  return combinations;
+};
+
+console.log(letterCombinations('2'))
